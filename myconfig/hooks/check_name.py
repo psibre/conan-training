c7dec514dd3012e2a7e0180c4decf12135f80fa5
Ingmar Steiner <isteiner@audeering.com>
2020-05-29 11:15:02 +0000

    Adapt for Ex.28

--- myconfig/hooks/check_name.py
+++ myconfig/hooks/check_name.py
@@ -5,5 +5,7 @@ def pre_export(output, conanfile, conanfile_path, reference, **kwargs):
     ref = str(reference)
     if ref.lower() != ref:
         raise Exception("Reference %s should be all lowercase" % ref)
+    if "-" in str(reference):
+        raise Exception("Reference %s should not contain a dash (-)" % ref)
     # use python "in" syntax.... if "something" in ref:
     #TODO
